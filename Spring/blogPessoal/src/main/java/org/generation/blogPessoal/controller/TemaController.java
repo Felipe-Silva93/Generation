3dfb5a0e64e514f19334f03a7235fa0333c3e7af
Felipe-Silva93 <felipesilva4127@gmail.com>
2021-06-08 22:18:04 -0300

    blog pessoal security

--- Spring/blogPessoal/src/main/java/org/generation/blogPessoal/controller/TemaController.java
+++ Spring/blogPessoal/src/main/java/org/generation/blogPessoal/controller/TemaController.java
@@ -34,7 +34,7 @@ public class TemaController {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Tema>getById(@PathVariable long id){
-		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+		return repository.findById(id).map/**/(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
